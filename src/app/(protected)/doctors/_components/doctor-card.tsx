3a0ae87e6f307258d3b04aa16548c7f3e0bfec83
Diegoh-dev <diegoh.developer@gmail.com>
2025-06-10 23:48:55 -0300

    feat: ensure patient can be created and updated

--- src/app/(protected)/doctors/_components/doctor-card.tsx
+++ src/app/(protected)/doctors/_components/doctor-card.tsx
@@ -113,6 +113,7 @@ const DoctorCard = ({ doctor }: DoctorCardProps) => {
               availableToTime: avalability.to.format("HH:mm:ss"),
             }}
             onSuccess={() => setIsUpsertDoctorDialogOpen(false)}
+            isOpen={isUpsertDoctorDialogOpen}
           />
         </Dialog>
 
